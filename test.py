e5443c4b69d0e2843a1933c63cd9541e431d0047
Asad Sayeed <asayeed@mbl.ca>
2019-02-25 01:29:59 +0100

    added test script

---
+++ test.py
@@ -0,0 +1,27 @@
+import os, sys
+import glob
+import argparse
+import numpy as np
+import pandas as pd
+
+# test.py -- Don't forget to put a reasonable amount code comments
+# in so that we better understand what you're doing when we grade!
+
+# add whatever additional imports you may need here.
+
+parser = argparse.ArgumentParser(description="Test a maximum entropy model.")
+parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
+parser.add_argument("datafile", type=str,
+                    help="The file name containing the features in the test data.")
+parser.add_argument("modelfile", type=str,
+                    help="The name of the saved model file.")
+
+args = parser.parse_args()
+
+print("Loading data from file {}.".format(args.datafile))
+print("Loading model from file {}.".format(args.modelfile))
+
+print("Testing {}-gram model.".format(args.ngram))
+
+print("Accuracy is ...")
+print("Perplexity is...")
